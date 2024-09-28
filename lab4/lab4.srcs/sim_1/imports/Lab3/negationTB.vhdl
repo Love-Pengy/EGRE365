5e32503fa099ff1030fd326b8c947e1b7fe4ab40
bee <afrazierboy2001@gmail.com>
2024-09-28 15:35:51 -0400

    negation and shift tb

--- lab4/lab4.srcs/sim_1/imports/Lab3/negationTB.vhdl
+++ lab4/lab4.srcs/sim_1/imports/Lab3/negationTB.vhdl
@@ -7,7 +7,7 @@ end entity;
 
 architecture behavior of adderTB is
   constant TIME_DELAY : time := 20 ns;
-  constant NUM_VALS : integer := 6;
+  constant NUM_VALS : integer := 3;
 
 
   type A_array is array(0 to (NUM_VALS - 1)) of std_logic_vector(15 downto 0);
@@ -19,52 +19,37 @@ architecture behavior of adderTB is
   type Cout_array is array(0 to (NUM_VALS - 1)) of std_logic;
   
   -- Expected input and output data.
-  -- Full Zeros
-  -- a negative and a positive 
-  -- a negative and a negative
-  -- a positive and negative in which the difference is zero 
-  -- overflows in the negative and positive direvtion 
-  -- a positive and a positive 
-
-  
+  -- full zeros
+  -- positive negation 
+  -- negative negation
+   
   constant A_vals : A_array := (B"0000_0000_0000_0000", 
-                                B"1000_1000_0000_0000",  -- -30720
-                                B"1111_0000_0000_0000", -- -4096 
-                                B"1111_1111_1111_1111", -- -1 
-                                B"1000_0000_0000_0000", -- -32768 
-                                B"0000_0000_0000_1000); -- 8 
-                                
-                                
+                                B"0000_0000_0001_0000",  
+                                B"1111_1111_1111_0000");  
                                 
   constant B_vals : B_array := (B"0000_0000_0000_0000",
-                                B"0000_0001_0001_0000", -- 272
-                                B"1000_0101_0000_0000", -- -31488 
-                                B"1111_1111_1111_1111", -- -1 
-                                B"1000_0000_0000_0001", -- -32767
-                                B"0000_0000_0000_0010); -- 2
+                                B"0000_0001_0001_0000", 
+                                B"1000_0101_0000_0000");  
                                 
                                 
                                 
-  constant mode_vals : mode_array := (B"001", 
-                                      B"001", 
-                                      B"001", 
-                                      B"001", 
-                                      B"001", 
-                                      B"001", 
-                                      B"001");
+  constant mode_vals : mode_array := (B"010", 
+                                      B"010", 
+                                      B"010", 
+                                      B"010", 
+                                      B"010", 
+                                      B"010", 
+                                      B"010");
                                       
-  constant Zero_vals : Zero_array := ('1','0','0', '1', '0', '0');
+  constant Zero_vals : Zero_array := ('1','0','0');
   
-  constant OE_vals : OE_array := ('1','1','1','1', '1', '1');
+  constant OE_vals : OE_array := ('1','1','1');
   
-  constant Cout_vals : Cout_array := ('0','0','0', '0', '1', '0');
+  constant Cout_vals : Cout_array := ('1','0','0');
   
   constant C_vals : C_array := (B"0000_0000_0000_0000",
-                                B"1000_0110_1111_0000", -- -30720 - 272 = 
-                                B"0110_1011_0000_0000", -- -4096 - -31488 
-                                B"0000_0000_0000_0000", -- -1 - -1 = -2
-                                B"1111_1111_1111_1111", -- -32768 - -32767 = -1
-                                B"0000_0000_0000_0110); -- 8 - 2 = 6
+                                B"1111_1111_1111_0000",  
+                                B"0000_0000_0001_0000"); 
                                 
 
   signal A_sig : std_logic_vector(15 downto 0);
