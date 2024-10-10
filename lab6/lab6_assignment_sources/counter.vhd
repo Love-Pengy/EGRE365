d68c6dca6720e9524b759825f2e4f326d18b2f30
Love-Pengy <afrazierboy2001@gmail.com>
2024-10-10 17:55:58 -0400

    update lab 6:

--- lab6/lab6_assignment_sources/counter.vhd
+++ lab6/lab6_assignment_sources/counter.vhd
@@ -11,10 +11,23 @@ entity counter is
 end counter ;
 
 architecture Behavioral of counter is
-   
+  
   begin
-
-  -- write process for an edge-triggered, 4-bit up
-  -- counter (with active high reset) here
-
+  
+    process(clk)
+    variable hold : std_logic_vector(3 downto 0) := (others => '0');
+    begin 
+        if(rising_edge(clk)) then 
+          if(rst = '1') then 
+            hold := B"0000";
+            count <= hold;
+          else
+            hold := std_logic_vector((unsigned(hold) + 1) MOD 4);
+            count <= hold;
+          end if;
+        end if;
+      -- write process for an edge-triggered, 4-bit up
+      -- counter (with active high reset) here
+    end process;
+    
 end Behavioral;
--- lab6/lab6_assignment_sources/d_ff_en.vhd
+++ lab6/lab6_assignment_sources/d_ff_en.vhd
@@ -13,8 +13,28 @@ END d_ff_en;
 ARCHITECTURE Behavioral OF d_ff_en IS
 
   BEGIN
-
+    
+    process(clk) 
+    begin 
+    -- 0 reset 1 set 
+        if(rising_edge(clk)) then
+           if(en = '1') then 
+              if(rst = '1') then 
+                  q <= '0';
+                  q <= '1';
+              end if;
+              else 
+                q <= d;
+                qn <= NOT d;
+            end if;
+            
+        end if;
+    
+    end process;
+    
+    
+    
   -- write process to implement an edger-triggered
   -- D flip flop with Enable (and active-high reset) here
   
-END behavior;
+END Behavioral;
